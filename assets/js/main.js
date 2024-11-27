a071a87d8c7ea00be2e8ab557538b52be3712305
prigicool12 <iqbalfahrozi284@gmail.com>
2024-11-27 13:12:54 +0700

    fix responsif

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+
+    // Saat hamburger diklik, toggle menu
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
